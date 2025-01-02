61ef6f061093af0d7e5bed2b9851e982f6dcc41a
Vadim Kiniabaev <thekinv21@gmail.com>
2025-01-02 11:48:51 +0300

    feat:[installation][Vadim]: Set global API prefix and add global exception filter and response interceptor

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,19 @@ import { AppModule } from './app.module'
 import * as dotenv from 'dotenv'
 
 import { SwaggerModule } from '@nestjs/swagger'
+import { ApiResponseInterceptor, GlobalExceptionFilter } from './base'
 import { swagger } from './config'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 
+	app.setGlobalPrefix('api')
+	app.useGlobalFilters(new GlobalExceptionFilter())
+	app.useGlobalInterceptors(new ApiResponseInterceptor())
+
 	SwaggerModule.setup('docs', app, SwaggerModule.createDocument(app, swagger))
 
-	await app.listen(process.env.PORT || 5200)
+	await app.listen(process.env.PORT || 4200)
 
 	dotenv.config()
 }
