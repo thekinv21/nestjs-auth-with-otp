6ff686ff771ff0e48fd3326d9d1d02ec489c4cd1
Vadim Kiniabaev <thekinv21@gmail.com>
2025-01-02 17:50:44 +0300

    feat:[otp]: Implement OtpService and update app module with global configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { OtpService } from './otp/otp.service'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true
+		})
+	],
+	controllers: [AppController],
+	providers: [AppService, OtpService]
 })
 export class AppModule {}
