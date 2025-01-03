00caa0cbe576ac0671b0f46b21e4a51447087246
Vadim Kiniabaev <thekinv21@gmail.com>
2025-01-03 18:11:00 +0300

    feat:[app-module]: Add CommonModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { CommonModule } from './common/common.module'
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true
-		})
+		}),
+		CommonModule
 	],
 	controllers: [],
-	providers: [CommonModule]
+	providers: []
 })
 export class AppModule {}
