0b5376bbe00389506efbcbfa255a34479fe75dad
Vadim Kiniabaev <thekinv21@gmail.com>
2025-01-03 16:21:25 +0300

    feat:[auth-otp][Vadim]: create otp crud

---
+++ src/common/otp/otp.controller.ts
@@ -0,0 +1,6 @@
+import { Controller } from '@nestjs/common'
+
+@Controller('otp')
+export class OtpController {
+	constructor() {}
+}
--- src/common/otp/otp.service.ts
+++ src/common/otp/otp.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
-export class OtpService {}
+export class OtpService {
+	constructor() {}
+}
